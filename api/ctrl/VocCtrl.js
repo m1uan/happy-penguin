076237d30da71f6942461faf90b0bccf67cdbca1
Milan Medlik <milan@morphoss.com>
2013-10-16 20:58:53 +1300

    add route from controllers

---
+++ api/ctrl/VocCtrl.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    index : function (request){
+        request.reply('ahoj')
+    }
+}
--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var Hapi = require('hapi');
+var Hapi = require('hapi'),
+    fs = require('fs'),
+    test = require('./api/ctrl/VocCtrl.js');
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
 var port      = 8080;
@@ -16,11 +18,60 @@ var hello = {
     }
 };
 
-// Add the route
-server.addRoute({
-    method : 'GET',
-    path : '/hello',
-    config : hello
+
+var DEF_CTRL = "Ctrl.js";
+
+function connectCtrl(ctrlpath, file){
+    var filePath = './' + ctrlpath + '/' + file;
+
+
+    if(filePath.indexOf(DEF_CTRL) == -1){
+        console.error('sorry but file \'' + filePath + '\' is not a Controller, (ctrl end by ...' + DEF_CTRL);
+        return;
+    } else {
+        console.info('controller found:' + filePath);
+    }
+
+    var module = require(filePath);
+
+    var ctrlname = '/' + (file.split(DEF_CTRL)[0]).toLowerCase();
+
+    for(var name in module){
+
+        var shandler = module[name];
+        //console.log('register' + file + '/' + name);
+        if(typeof shandler === 'function'){
+            var connect = ctrlname + '/' + name;
+            console.log('register:' + connect);
+
+            var superhello = {
+                handler: shandler
+            };
+
+            // Add the route
+            server.addRoute({
+                method : 'GET',
+                path : connect,
+                config : superhello
+            });
+        }
+
+    }
+
+}
+
+
+var ctrlpath = 'api/ctrl';
+fs.exists(ctrlpath, function(exists){
+   if(exists){
+
+       var files = fs.readdirSync(ctrlpath);
+       for(var file in files){
+           connectCtrl(ctrlpath, files[file]);
+       }
+   }
+
+
 });
 
 console.log(ipaddress + ':' +port);
