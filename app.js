013088e60567126ee09521eb3dd367744c7698a4
Milan Medlik <milan@morphoss.com>
2013-10-16 19:45:55 +1300

    test 3

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var Hapi = require('hapi')
     , fs = require('fs');
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 
 console.log('starting');
 
 // Create a server with a host, port, and options
-var server = new Hapi.Server();
+var server = new Hapi.Server(ipaddress, port);
 
 // Define the route
 var hello = {
