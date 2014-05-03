4badd3541b1a00fb0e69d47635d57d9e3b6659a8
m1uan <milan.medlik@gmail.com>
2014-05-03 15:54:30 +1200

    get entry just if you have enought experiences

--- assets/js/pinguin/pinguin-app.js
+++ assets/js/pinguin/pinguin-app.js
@@ -92,18 +92,21 @@ function WorldCtrl($scope, $location, $http, localStorageService) {
     //element.click(onClick);
     update();
 
+    $scope.fly = 10;
+    $scope.walk = 10;
+    $scope.swim = 10;
+    $scope.exp = 10;
+
     var game = localStorageService.get('pinguin.game');
 
     self.generateInfo = function(place){
         var place_popover = $('#place_popover');
         place_popover.find('#place_popover_posx').text(place.posx);
         place_popover.find('#place_popover_posy').text(place.posy);
-
-
-        var xd = game.posx - place.posx;
-        var yd = game.posy - place.posy;
-        var distance = Math.sqrt((xd*xd)+(yd*yd));
-        place_popover.find('#place_popover_distance').text(distance);
+        place_popover.find('#place_popover_distance').text(place.superDistance);
+        place_popover.find('#place_popover_fly').text(place.fly);
+        place_popover.find('#place_popover_swim').text(place.swim);
+        place_popover.find('#place_popover_walk').text(place.walk);
         return place_popover.html();
     }
 
@@ -120,13 +123,40 @@ function WorldCtrl($scope, $location, $http, localStorageService) {
                 var left = parseFloat(1350) * parseFloat(pl.posx);
                 var top = parseFloat(675) * parseFloat(pl.posy);
 
+                var xd = game.posx - pl.posx;
+                var yd = game.posy - pl.posy;
+                var distance = Math.sqrt((xd*xd)+(yd*yd));
+
+
+                pl.superDistance = Math.round(distance*100);
+
+                pl.fly = Math.round(pl.superDistance / 9);
+                pl.swim = Math.round((pl.superDistance - (pl.fly*6)) / 3);
+                pl.walk = (pl.superDistance - (pl.fly*5) - (pl.swim*2));
+
+
                 item.css({top: top, left: left});
                 item.appendTo(element);
                 item.click(function(){
-                    console.log('click');
-                    $scope.$apply(function(){
-                        $location.path('/place/'+pl.id);
-                    })
+                    if($scope.fly < pl.fly){
+                        alertify.error('Sorry you have enought FLY... you have just ' + $scope.fly+ ' but you need at least '+pl.fly);
+                        $('#game_resources_fly').css({color:'red'});
+                    } else if($scope.swim < pl.swim){
+                        alertify.error('Sorry you have enought SWIM... you have just ' + $scope.swim+ ' but you need at least '+pl.swim);
+                        $('#game_resources_swim').css({color:'red'});
+                    } else if($scope.walk < pl.walk){
+                        alertify.error('Sorry you have enought WALK... you have just ' + $scope.walk+ ' but you need at least '+pl.walk);
+                        $('#game_resources_walk').css({color:'red'});
+                    } else {
+                        $scope.$apply(function(){
+                            $scope.fly -= pl.fly;
+                            $scope.swim -= pl.swim;
+                            $scope.walk -= pl.walk;
+
+                            //$location.path('/place/'+pl.id);
+                        })
+                    }
+
 
                 });
 
