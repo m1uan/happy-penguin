07fc0d8a0e81b184585cb3e4d017ef7b6730dcb7
m1uan <milan.medlik@gmail.com>
2014-09-26 20:31:24 +1200

    create table for info

---
+++ initdata/update_v1.9.sql
@@ -0,0 +1,27 @@
+DROP TABLE IF EXISTS pinguin.place_info_type_t;
+DROP TABLE IF EXISTS pinguin.place_info_t;
+
+CREATE TABLE pinguin.place_info_type_t (
+    pit SERIAL NOT NULL,
+    name TEXT NOT NULL,
+    PRIMARY KEY(pit)
+);
+
+INSERT INTO pinguin.place_info_type_t (name) VALUES
+    ('City'),
+    ('Joke'),
+    ('Quote'),
+    ('Battle'),
+    ('History'),
+    ('Story');
+
+CREATE TABLE pinguin.place_info_t (
+    pi SERIAL NOT NULL,
+    name INTEGER NOT NULL,
+   "info" INTEGER DEFAULT NULL,
+   "type" INTEGER NOT NULL DEFAULT 1,
+    FOREIGN KEY (name) REFERENCES translates.link_t (link),
+    FOREIGN KEY (info) REFERENCES translates.link_t (link),
+    FOREIGN KEY (type) REFERENCES pinguin.place_info_type_t (pit),
+    PRIMARY KEY (pi)
+)
